f6ae59b6e363ad023593efb3bb08f847772d9c98
jjac111 <javier_arosemena@hotmail.com>
2020-12-14 18:40:46 -0500

    changed dns ip

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-dns_ip = '192.168.0.22'
+dns_ip = '192.168.0.29'
 my_ip = '0.0.0.0'
 mitm_ip = '0.0.0.0'
 
-ports = {'adrian': 65431, 'diego': 65432, 'dns': 65430, 'mitm':65433}
+ports = {'adrian': 65430, 'diego': 65430, 'dns': 65430, 'mitm':65430}
 privates = {'adrian': 2484, 'diego': 77}
 
 msg_length = 100
