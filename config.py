79c19420a6824c9e3f11c0e6ad37f913bd60711c
jjac111 <javier_arosemena@hotmail.com>
2020-12-14 16:24:17 -0500

    changed ip from localhost to 0.0.0.0

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 dns_ip = '192.168.0.22'
-my_ip = '127.0.0.1'
-mitm_ip = '127.0.0.1'
+my_ip = '0.0.0.0'
+mitm_ip = '0.0.0.0'
 
 ports = {'adrian': 65431, 'diego': 65432, 'dns': 65430, 'mitm':65433}
 privates = {'adrian': 2484, 'diego': 77}
